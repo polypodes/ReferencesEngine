ae49d481320ab8ca540b5e94f4fe032f842adf3a
Charlie <charlielucas.cl@gmail.com>
2014-05-13 17:47:38 +0200

    Commit avant test sur l API

--- src/Application/Refactor/ReferenceBundle/ApplicationRefactorReferenceBundle.php
+++ src/Application/Refactor/ReferenceBundle/ApplicationRefactorReferenceBundle.php
@@ -3,7 +3,16 @@
 namespace Application\Refactor\ReferenceBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Application\Refactor\ReferenceBundle\DependencyInjection\Security\Factory\WsseFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ApplicationRefactorReferenceBundle extends Bundle
 {
+	// public function build(ContainerBuilder $container)
+ //    {
+ //        parent::build($container);
+
+ //        $extension = $container->getExtension('security');
+ //        $extension->addSecurityListenerFactory(new WsseFactory());
+ //    }
 }
---
+++ src/Application/Refactor/ReferenceBundle/Security/Authentication/Token/WsseUserToken.php
@@ -0,0 +1,22 @@
+<?php
+namespace Application\Refactor\ReferenceBundle\Security\Authentication\Token;
+
+use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
+
+class WsseUserToken extends AbstractToken
+{
+    public $created;
+    public $digest;
+    public $nonce;
+
+    public function __construct(array $roles = array())
+    {
+        parent::__construct($roles);
+        $this->setAuthenticated(count($roles) > 0);
+    }
+
+    public function getCredentials()
+    {
+        return '';
+    }
+}
